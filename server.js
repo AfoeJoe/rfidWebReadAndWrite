d7af06d1afbcf77162398bae4c9c649c723b25ba
kingjamesegun <kingjamesegun@gmail.com>
2021-06-28 15:55:48 +0300

    changed the hostname

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const { read, write } = require("./utils.js");
-const PORT = 3000;
-const HOSTNAME = "192.168.0.20";
+const express = require('express');
+const { read, write } = require('./utils.js');
+const PORT = process.env.PORT || 3000;
+//const HOSTNAME = '192.168.0.20';
 const app = express();
 
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
   cors: {
     origin: HOSTNAME,
-    methods: ["GET", "POST"],
-    transports: ["websocket", "polling"],
+    methods: ['GET', 'POST'],
+    transports: ['websocket', 'polling'],
     credetials: true,
   },
   allowEIO3: true,
 });
-app.use(express.static("./"));
+app.use(express.static('./'));
 
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
-io.on("connection", (socket) => {
-  socket.emit("connection", "reads");
-  socket.on("read", (data) => read(data, io));
-  socket.on("write", (data) => write(data, io));
+io.on('connection', (socket) => {
+  socket.emit('connection', 'reads');
+  socket.on('read', (data) => read(data, io));
+  socket.on('write', (data) => write(data, io));
 });

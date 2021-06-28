381b3c7a3994297aed17aaac26e39e61fb286f14
kingjamesegun <kingjamesegun@gmail.com>
2021-06-28 17:33:37 +0300

    h

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-"use strict";
-const Mfrc522 = require("mfrc522-rpi");
-const SoftSPI = require("rpi-softspi");
+'use strict';
+const Mfrc522 = require('mfrc522-rpi');
+const SoftSPI = require('rpi-softspi');
 const softSPI = new SoftSPI({
   clock: 23, // pin number of SCLK
   mosi: 19, // pin number of MOSI
@@ -14,16 +14,16 @@ const mfrc522 = new Mfrc522(softSPI).setResetPin(22).setBuzzerPin(18);
 
 function read(data, io) {
   //# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-  console.log("scanning...");
-  console.log("Please put chip or keycard in the antenna inductive zone!");
-  console.log("Press Ctrl-C to stop.");
+  console.log('scanning...');
+  console.log('Please put chip or keycard in the antenna inductive zone!');
+  console.log('Press Ctrl-C to stop.');
 
   // GPIO 24 can be used for buzzer bin (PIN 18), Reset pin is (PIN 22).
   // I believe that channing pattern is better for configuring pins which are optional methods to use.
   let timerId = setInterval(scan, 200);
   let timeout = setTimeout(() => {
     clearInterval(timerId);
-    io.sockets.emit("error", { messagee: "timeout" });
+    io.sockets.emit('error', { messagee: 'timeout' });
   }, 10000);
 
   function scan() {
@@ -33,21 +33,22 @@ function read(data, io) {
     //# Scan for cards
     let response = mfrc522.findCard();
     if (!response.status) {
-      console.log("No Card");
+      console.log(response.status);
+      console.log('No Card');
       return;
     }
-    console.log("Card detected, CardType: " + response);
+    console.log('Card detected, CardType: ' + response);
 
     //# Get the UID of the card
     response = mfrc522.getUid();
     if (!response.status) {
-      io.sockets.emit("error", { messagee: "UID Scan Error" })
+      io.sockets.emit('error', { messagee: 'UID Scan Error' });
       return;
     }
     //# If we have the UID, continue
     const uid = response.data;
     console.log(
-      "Card read UID: %s %s %s %s",
+      'Card read UID: %s %s %s %s',
       uid[0].toString(16),
       uid[1].toString(16),
       uid[2].toString(16),
@@ -62,7 +63,7 @@ function read(data, io) {
 
     //# Authenticate on Block 8 with key and uid
     if (!mfrc522.authenticate(8, key, uid)) {
-      io.sockets.emit("error", { messagee: "Authentication error" })
+      io.sockets.emit('error', { messagee: 'Authentication error' });
       return;
     }
 
@@ -74,14 +75,14 @@ function read(data, io) {
     clearInterval(timerId);
     clearTimeout(timeout);
 
-    io.sockets.emit("detected", {
+    io.sockets.emit('detected', {
       uid: [
         uid[0].toString(16),
         uid[1].toString(16),
         uid[2].toString(16),
         uid[3].toString(16),
       ],
-      "read data": hexToString(textRead),
+      'read data': hexToString(textRead),
     });
     return true;
   }
@@ -89,9 +90,9 @@ function read(data, io) {
 
 function write(data, io) {
   //# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-  console.log("scanning...");
-  console.log("Please put chip or keycard in the antenna inductive zone!");
-  console.log("Press Ctrl-C to stop.");
+  console.log('scanning...');
+  console.log('Please put chip or keycard in the antenna inductive zone!');
+  console.log('Press Ctrl-C to stop.');
 
   //# reset card
   mfrc522.reset();
@@ -103,7 +104,7 @@ function write(data, io) {
       let flag = false;
       let timeout = setTimeout(() => {
         clearInterval(timeout);
-        reject("error");
+        reject('error');
       }, 10000);
 
       let timerid = setInterval(() => {
@@ -124,13 +125,13 @@ function write(data, io) {
       //# Get the UID of the card
       response = mfrc522.getUid();
       if (!response.status) {
-        io.sockets.emit("error", { messagee:  "UID Scan Error"})
+        io.sockets.emit('error', { messagee: 'UID Scan Error' });
         return;
       }
       //# If we have the UID, continue
       const uid = response.data;
       console.log(
-        "Card read UID: %s %s %s %s",
+        'Card read UID: %s %s %s %s',
         uid[0].toString(16),
         uid[1].toString(16),
         uid[2].toString(16),
@@ -145,24 +146,24 @@ function write(data, io) {
 
       //# Authenticate on Block 8 with key and uid
       if (!mfrc522.authenticate(8, key, uid)) {
-        io.sockets.emit("error", { messagee: "Authentication error" })
+        io.sockets.emit('error', { messagee: 'Authentication error' });
         return;
       }
- 
+
       let datatoWrite = new Int16Array(
-        data.text.split("").map((x) => x.charCodeAt(0))
+        data.text.split('').map((x) => x.charCodeAt(0))
       );
       mfrc522.writeDataToBlock(8, datatoWrite);
       mfrc522.stopCrypto();
-      io.sockets.emit("success", { message: "written" });
+      io.sockets.emit('success', { message: 'written' });
     })
     .catch((err) =>
-      io.sockets.emit("error", { messagee: "timeout,no card read" })
+      io.sockets.emit('error', { messagee: 'timeout,no card read' })
     );
 }
 
 function hexToString(arr) {
-  let res = "";
+  let res = '';
   for (let i = 0; i < arr.length; i++) {
     res += String.fromCharCode(parseInt(arr[i]));
   }

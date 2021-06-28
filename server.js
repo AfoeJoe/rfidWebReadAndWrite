a3a3b78016ee089fbcd87632bfdfbd89dae7aa76
Afoejoe <afoejoe@gmail.com>
2021-06-28 14:23:30 +0100

    added gitignore

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { read, write } = require('./utils.js');
 const PORT = process.env.PORT || 3000;
-//const HOSTNAME = '192.168.0.20';
+const HOSTNAME = '192.168.0.20';
 const app = express();
 
 const server = require('http').createServer(app);
